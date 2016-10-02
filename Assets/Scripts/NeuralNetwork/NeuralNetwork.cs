08333534926dffc95ee0784f5dc9588fddbf92e4
AntonPendiur <a.pendiur@pgames.guru>
2016-10-02 14:06:49 +0300

    Add bias neuron.

--- Assets/Scripts/NeuralNetwork/NeuralNetwork.cs
+++ Assets/Scripts/NeuralNetwork/NeuralNetwork.cs
@@ -14,15 +14,15 @@ public class NeuralNetwork {
 		_layerSizes = layerSizes;
 		_weights = new double[LayersCount-1][,];
 		for (int layerInd=0;layerInd<_weights.Length;layerInd++) {
-			_weights[layerInd] = new double[layerSizes[layerInd],layerSizes[layerInd+1]];
-			for (int neuronInd1 = 0; neuronInd1 < _layerSizes [layerInd]; neuronInd1++) {
+			_weights[layerInd] = new double[layerSizes[layerInd]+1,layerSizes[layerInd+1]];
+			for (int neuronInd1 = 0; neuronInd1 < _layerSizes [layerInd]+1; neuronInd1++) {
 				for (int neuronInd2 = 0; neuronInd2 < _layerSizes [layerInd + 1]; neuronInd2++)
 					_weights [layerInd][neuronInd1, neuronInd2] = (double)Game.RandomValue;
 			}
 		}
 		_neuronValues = new double[LayersCount][];
 		for (int layerInd=0;layerInd<LayersCount;layerInd++)
-			_neuronValues[layerInd] = new double[_layerSizes[layerInd]];
+			_neuronValues[layerInd] = new double[_layerSizes[layerInd]+1];
 	}
 
 	[NonSerialized]
@@ -31,13 +31,18 @@ public class NeuralNetwork {
 		System.Diagnostics.Debug.Assert (inputs.Length == _layerSizes [0]);
 		for (int layerInd = 0; layerInd < _weights.Length; layerInd++) {
 			for (int neuronInd = 0; neuronInd < _layerSizes [layerInd]; neuronInd++) {
-				_neuronValues [layerInd][neuronInd] = layerInd==0?inputs[neuronInd]:0;
+				if (neuronInd == _layerSizes [layerInd])
+					_neuronValues [layerInd] [neuronInd] = 1; // Bias neuron.
+				else if (layerInd == 0)
+					_neuronValues [0][neuronInd] = inputs[neuronInd]; // Retina neuron.
+				else
+					_neuronValues [layerInd][neuronInd] = 0; // Other neuron.
 			}
 		}
 		for (int layerInd = 1; layerInd < LayersCount; layerInd++) {
 			for (int neuronInd1 = 0; neuronInd1 < _layerSizes [layerInd]; neuronInd1++) {
 				// Sum values.
-				for (int neuronInd2 = 0; neuronInd2 < _layerSizes [layerInd-1]; neuronInd2++)
+				for (int neuronInd2 = 0; neuronInd2 < _layerSizes [layerInd-1]+1; neuronInd2++)
 					_neuronValues [layerInd] [neuronInd1] += _weights [layerInd-1] [neuronInd2, neuronInd1] * _neuronValues [layerInd - 1][neuronInd2];
 				// Apply activation function.
 				_neuronValues[layerInd][neuronInd1] = FAct(_neuronValues[layerInd][neuronInd1]);
